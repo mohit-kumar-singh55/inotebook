06c7433acc9743e20e1e3198e67cd121752b1ff6
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2021-12-06 19:48:14 +0530

    Created Model for Mongo and Routes

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,14 @@ const connectToMango = require('./db');
 
 connectToMango();
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// Available Routes
+app.use("api/auth", require("./routes/auth"));
+app.use("api/notes", require("./routes/notes"));
+
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
