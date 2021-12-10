6a9bfb256e8520173be5d905607d8e20b050dfb2
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2021-12-10 20:15:21 +0530

    Setted up Router Dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { About } from './components/About';
+import Home from './components/Home';
+import { Navbar } from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar title="iNotebook" />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
